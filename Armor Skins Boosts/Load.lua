b508f74ff65c64397cb7cb88c0264b5d21a84c23
Dr_Newbie <tf2baidonation@gmail.com>
2017-08-23 15:11:23 +0800

    12 ; Armor Regen

--- Armor Skins Boosts/Load.lua
+++ Armor Skins Boosts/Load.lua
@@ -1,21 +1,24 @@
 function Load_ArmorSkinsBoosts()
-	local _DATA_PATH = "mods/Armor Skins Boosts/data/"
-	local _files = file.GetFiles(_DATA_PATH) or {}
-	for _, _file_name in pairs(_files) do
-		local _file = io.open(_DATA_PATH .. _file_name, "r")
-		local _data = {}
-		if _file then
-			_data = json.decode(_file:read("*all"))
-			_file:close()
-			tweak_data.economy.armor_skins[_file_name] = tweak_data.economy.armor_skins[_file_name] or {}
-			tweak_data.economy.armor_skins[_file_name].body_armor = tweak_data.economy.armor_skins[_file_name].body_armor or {}
-			tweak_data.economy.armor_skins[_file_name].body_armor = _data
-		end
+	if tweak_data and tweak_data.economy and tweak_data.economy.armor_skins then
+		local _DATA_PATH = "mods/Armor Skins Boosts/data/"
+		local _files = file.GetFiles(_DATA_PATH) or {}
+		for _, _file_name in pairs(_files) do
+			local _file = io.open(_DATA_PATH .. _file_name, "r")
+			local _data = {}
+			if _file then
+				_data = json.decode(_file:read("*all"))
+				_file:close()
+				tweak_data.economy.armor_skins[_file_name] = tweak_data.economy.armor_skins[_file_name] or {}
+				tweak_data.economy.armor_skins[_file_name].body_armor = tweak_data.economy.armor_skins[_file_name].body_armor or {}
+				tweak_data.economy.armor_skins[_file_name].body_armor = _data
+			end
+		end		
 	end
 end
 
-if tweak_data and tweak_data.economy and tweak_data.economy.armor_skins then
-	Load_ArmorSkinsBoosts()
+
+function Get_Current_ArmorSkinsBoosts()
+	return tweak_data.economy.armor_skins[tostring(managers.blackmarket:equipped_armor_skin())] or {}
 end
 
-log(tostring(json.encode({dodge = {0.03, 0.03, 0.04, 0.04, 0.05, 0.05, 0.06}})))
+Load_ArmorSkinsBoosts()
--- Armor Skins Boosts/lua/armorskinstweakdata.lua
+++ Armor Skins Boosts/lua/armorskinstweakdata.lua
@@ -2,12 +2,13 @@ dofile("mods/Armor Skins Boosts/Load.lua")
 
 Hooks:Add("LocalizationManagerPostInit", "ArmorBoosts_loc", function(loc)
 	local _INFO = {
-		["armor_skins_boost_dodge"] = "- Dodge +%d%%",
-		["armor_skins_boost_concealment"] = "- Concealment +%d",
-		["armor_skins_boost_movement"] = "- Movement +%d%%",
-		["armor_skins_boost_armor"] = "- Armor +%d",
-		["armor_skins_boost_stamina"] = "- Stamina +%d%%",
-		["armor_skins_boost_damage_shake"] = "- Shake +%d%%"
+		["armor_skins_boost_dodge"] = "- Dodge +%s%%",
+		["armor_skins_boost_concealment"] = "- Concealment +%s",
+		["armor_skins_boost_movement"] = "- Movement +%s%%",
+		["armor_skins_boost_armor"] = "- Armor +%s",
+		["armor_skins_boost_stamina"] = "- Stamina +%s%%",
+		["armor_skins_boost_damage_shake"] = "- Shake +%s%%",
+		["armor_skins_boost_armor_regen"] = "- Armor Regen -%s%%"
 	}
 	local _DESC_VAULE_FIX = {
 		dodge = 100,
@@ -15,7 +16,8 @@ Hooks:Add("LocalizationManagerPostInit", "ArmorBoosts_loc", function(loc)
 		movement = 100,
 		armor = 1,
 		stamina = 100,
-		damage_shake = 100
+		damage_shake = 100,
+		armor_regen = 100
 	}
 	for skins_name, skins_datas in pairs(tweak_data.economy.armor_skins) do
 		if skins_datas and skins_datas.body_armor then
@@ -24,7 +26,7 @@ Hooks:Add("LocalizationManagerPostInit", "ArmorBoosts_loc", function(loc)
 				_desc_data = _desc_data .. "\n(".. loc:text("bm_armor_level_" .. i) ..")" 
 				for mod_name, mod_data in pairs(skins_datas.body_armor) do
 					if skins_datas.body_armor[mod_name] then
-						_desc_data = _desc_data .. " - " ..string.format(_INFO["armor_skins_boost_" .. mod_name], math.round(mod_data[i]*(_DESC_VAULE_FIX[mod_name] or 1)))
+						_desc_data = _desc_data .. " - " ..string.format(tostring(_INFO["armor_skins_boost_" .. mod_name]), tostring(math.round(mod_data[i]*(_DESC_VAULE_FIX[mod_name] or 1))))
 					end
 				end
 				_desc_data = _desc_data .. "\n"
