13910f008a81ed70f8b14aa89f08469ad565a987
Dr_Newbie <tf2baidonation@gmail.com>
2017-08-23 14:30:12 +0800

    8+9

--- Armor Skins Boosts/lua/armorskinstweakdata.lua
+++ Armor Skins Boosts/lua/armorskinstweakdata.lua
@@ -3,11 +3,19 @@ dofile("mods/Armor Skins Boosts/Load.lua")
 Hooks:Add("LocalizationManagerPostInit", "ArmorBoosts_loc", function(loc)
 	local _INFO = {
 		["armor_skins_boost_dodge"] = "- Dodge +%d%%",
-		["armor_skins_boost_concealment"] = "- Concealment +%d%%",
-		["armor_skins_boost_movement"] = "- Movement +%d%%",
-		["armor_skins_boost_armor"] = "- Armor +%d%%",
+		["armor_skins_boost_concealment"] = "- Concealment +%d",
+		["armor_skins_boost_movement"] = "- Movement +%d",
+		["armor_skins_boost_armor"] = "- Armor +%d",
 		["armor_skins_boost_stamina"] = "- Stamina +%d%%",
 		["armor_skins_boost_damage_shake"] = "- Shake +%d%%"
+	}
+	local _DESC_VAULE_FIX = {
+		dodge = 100,
+		concealment = 1,
+		movement = 1,
+		armor = 1,
+		stamina = 100,
+		damage_shake = 100
 	}
 	for skins_name, skins_datas in pairs(tweak_data.economy.armor_skins) do
 		if skins_datas and skins_datas.body_armor then
@@ -16,7 +24,7 @@ Hooks:Add("LocalizationManagerPostInit", "ArmorBoosts_loc", function(loc)
 				_desc_data = _desc_data .. "\n(".. loc:text("bm_armor_level_" .. i) ..")" 
 				for mod_name, mod_data in pairs(skins_datas.body_armor) do
 					if skins_datas.body_armor[mod_name] then
-						_desc_data = _desc_data .. " - " ..string.format(_INFO["armor_skins_boost_" .. mod_name], math.round(mod_data[i]*100))
+						_desc_data = _desc_data .. " - " ..string.format(_INFO["armor_skins_boost_" .. mod_name], math.round(mod_data[i]*(_DESC_VAULE_FIX[mod_name] or 1)))
 					end
 				end
 				_desc_data = _desc_data .. "\n"
